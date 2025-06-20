b4c92f0fed43a173926fe6f5879c9461738f65b6
Deepankar Tiwari <dtiwari@configit.com>
2025-06-20 15:34:31 +0530

    Adding dummy secret

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Defender Scan test image!"
+
+# Dummy GitHub token: ghp_FAKE1234567890EXAMPLETOKEN
+# Dummy Azure secret: DefaultEndpointsProtocol=https;AccountName=fake;AccountKey=abc123==
+# Dummy password: password=MySecret123!
